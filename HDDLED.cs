adbf6fb37550adbdbe22f73ca0e4b5e7bf5bbfc6
fergoman123 <mickferg1@gmail.com>
2015-03-09 19:11:56 +1100

    Inital Setup
    Signed-off-by: fergoman123 <mickferg1@gmail.com>

---
+++ HDDLED.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HDDLED_v2
+{
+    static class HDDLED
+    {
+        /// <summary>
+        /// the main entry point for the application
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new InvisableForm());
+        }
+    }
+}
