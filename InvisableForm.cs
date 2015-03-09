bc12c1740c0c2c58de2cbfe03e9ddf7956ea667f
fergoman123 <mickferg1@gmail.com>
2015-03-09 19:29:44 +1100

    Added CreateParams override

--- InvisableForm.cs
+++ InvisableForm.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.Management.Instrumentation;
+using System.Collections.Specialized;
+using System.Threading;
 
 namespace HDDLED_v2
 {
     public partial class InvisableForm : Form
     {
+
+        #region Members
+        NotifyIcon hddNotifyIcon;
+        Icon busyIcon;
+        Icon idleIcon;
+        Thread hddInfoWorker;
+        #endregion
+
+        #region Main Form (Entry Point)
         public InvisableForm()
         {
             InitializeComponent();
         }
+        #endregion
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams pm = base.CreateParams;
+                pm.ExStyle |= 0x80;
+                return pm;
+            }
+        }
+
     }
 }
